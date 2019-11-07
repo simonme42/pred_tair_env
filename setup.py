6993215931087d6934342374bf899279d1e5c868
simonme42 <57465172+simonme42@users.noreply.github.com>
2019-11-08 00:39:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ from setuptools import setup
 
 setup(name='pred_tair_env',
       version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'hfo_py>=0.2']
+      install_requires=['gym>=0.2.3']
 )
