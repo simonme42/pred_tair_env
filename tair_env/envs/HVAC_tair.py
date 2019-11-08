5968a20ec4065b7b3a578a09ed1c97c7df747651
simonme42 <57465172+simonme42@users.noreply.github.com>
2019-11-08 12:37:57 +0100

    Update HVAC_tair.py

--- tair_env/envs/HVAC_tair.py
+++ tair_env/envs/HVAC_tair.py
@@ -8,16 +8,16 @@ import numpy as np
 class pred_Tair_env(gym.Env):
     metadata = {'render.modes': ['human']}
   
-    def __init__(self, data, all_data, start_pred_index, min_reward, max_reward, n_actions, 
+    def __init__(self, test_data, all_data, start_pred_index, y_scaler_Tair, y_scaler_Energy, 
+                model_Tair, model_Energy_input, min_reward=-25, max_reward=0, n_actions=22, 
                 max_steps=168, Hourofday_idx, Tamb_idx, Rad_hor_idx, 
                 pred_Tair_idx, Belegung_idx, Setpoint_Temp_idx,
-                Air_flow_rate_idx, y_scaler_Tair, y_scaler_Energy, 
-                model_Tair, model_Energy_input, Solltemp=21, Energy_divident=500):
+                Air_flow_rate_idx, Solltemp=21, Energy_divident=500):
         
         super(pred_Tair_env, self).__init__()
         
-        self.data = np.copy(data)
-        self.all_data = all_data
+        self.data = np.copy(test_data)
+        self.all_data = np.copy(all_data)
         self.state = np.copy(self.data[0:1])
         self.reward_range = (min_reward, max_reward) 
         self.action_space = spaces.Discrete(n_actions)
@@ -26,50 +26,47 @@ class pred_Tair_env(gym.Env):
                                             dtype=np.float32)
         ##create the possible actions the agent can take inside the environment
         possible_actions = list((np.arange(19,41)-20)/20)
-
         for i in range(n_actions):
             if i == 0:
                 possible_actions[i] = [0.0, 0.0]
             else:
                 possible_actions[i] = [possible_actions[i], 1]
-
-        self.possible_actions = possible_actions[:]
+        self.possible_actions = possible_actions
 
     def reset(self):        
         self.current_step = 0 
         # Reset the state of the environment to the initial state
         self.state = np.copy(self.data[self.current_step:self.current_step+1])
         self.current_simtime = start_pred_index + 3 + self.current_step
-        self.current_daytime = all_data[self.current_simtime][Hourofday_idx]
-        self.future_Tamb = self.data[self.current_step:self.current_step+6, 2, Tamb_idx]
-        self.future_Rad = self.data[self.current_step:self.current_step+6, 2, Rad_hor_idx]
-        tair_after_step_scaled = np.copy(self.state[0, 2, pred_Tair_idx])
+        self.current_daytime = np.copy(all_data[self.current_simtime][Hourofday_idx])
+        self.future_Tamb = np.copy(self.data[self.current_step:self.current_step+1, 2, Tamb_idx])
+        self.future_Rad = np.copy(self.data[self.current_step:self.current_step+1, 2, Rad_hor_idx])
         
-        occupation = self.state[0, 2, Belegung_idx]
-        
-        raw_obs = self.state[0,-1:,:]        
+        self.tair_at_reset = np.copy(self.state[0, 2, pred_Tair_idx])
+        self.occupation = np.copy(self.state[0, 2, Belegung_idx])
+              
         obs = np.zeros((1, 5))
-        
-        #if self.current_daytime > occupy_start and self.current_daytime <= occupy_end:
-            #occupation += 1
 
         for i in range(1):
-            obs[i, 0] = tair_after_step_scaled
-            obs[i, 1] = occupation
+            obs[i, 0] = self.tair_at_reset
+            obs[i, 1] = self.occupation
             obs[i, 2] = (self.current_daytime-1) / 23  #scale between 0 and 1
             obs[i, 3] = np.copy(self.future_Tamb[0])
-            obs[i, 4] = np.copy(self.future_Rad[0])
-        
+            obs[i, 4] = np.copy(self.future_Rad[0])        
         
         return obs
                                 
     def step(self, action):
         # Execute one time step within the environment
+        
         control_params = possible_actions[action]
-        heating_IO = control_params[1]
+        Setpoint_temp = control_params[0]
+        Heating_IO = control_params[1]
+        
         self.previous_state = np.copy(self.state[:,:,:])
-        self.previous_state[0, 2, Setpoint_Temp_idx] = control_params[0]
-        self.previous_state[0, 2, Air_flow_rate_idx] = control_params[1]
+        self.previous_state[0, 2, Setpoint_Temp_idx] = Setpoint_temp
+        self.previous_state[0, 2, Air_flow_rate_idx] = Heating_IO
+        
         energy_input_unscaled = model_Energy_input.predict(self.previous_state)
         energy_input = y_scaler_Energy.inverse_transform(energy_input_unscaled)
         self.energy_input_reward = energy_input/Energy_divident
@@ -91,7 +88,7 @@ class pred_Tair_env(gym.Env):
  
         obs = np.zeros((1, 5))
         
-        for i in range(n_steps_obs):
+        for i in range(1):
             obs[i, 0] = tair_after_step_scaled
             obs[i, 1] = occupation
             obs[i, 2] = (self.current_daytime-1) / 23  #scale between 0 and 1
@@ -113,24 +110,24 @@ class pred_Tair_env(gym.Env):
     
     def _take_action(self, action):
         control_action = possible_actions[action]
-        setpoint_temp = control_action[0]
-        air_flow_IO = control_action[1]
+        Setpoint_temp = control_action[0]
+        Heating_IO = control_action[1]
                 
         if self.current_step == 0:        
-            self.state[0, 2, Setpoint_Temp_idx] = setpoint_temp
-            self.state[0, 2, Air_flow_rate_idx] = air_flow_IO
+            self.state[0, 2, Setpoint_Temp_idx] = Setpoint_temp
+            self.state[0, 2, Air_flow_rate_idx] = Heating_IO
             tair_next_step = model_Tair.predict(self.state)
             
             next_state = self.data[self.current_step+1:self.current_step+2]
-            next_state[0, 1, Setpoint_Temp_idx] = setpoint_temp
-            next_state[0, 1, Air_flow_rate_idx] = air_flow_IO
+            next_state[0, 1, Setpoint_Temp_idx] = Setpoint_temp
+            next_state[0, 1, Air_flow_rate_idx] = Heating_IO
             next_state[0, 2, pred_Tair_idx] = tair_next_step
             
             self.state = next_state
             
         elif self.current_step == 1:
-            self.state[0, 2, Setpoint_Temp_idx] = setpoint_temp
-            self.state[0, 2, Air_flow_rate_idx] = air_flow_IO
+            self.state[0, 2, Setpoint_Temp_idx] = Setpoint_temp
+            self.state[0, 2, Air_flow_rate_idx] = Heating_IO
 
             tair_next_step = model_Tair.predict(self.state)
             
@@ -138,15 +135,15 @@ class pred_Tair_env(gym.Env):
             next_state[0, 0, Setpoint_Temp_idx] = self.state[0, 1, Setpoint_Temp_idx]
             next_state[0, 0, Air_flow_rate_idx] = self.state[0, 1, Air_flow_rate_idx]
             next_state[0, 1, pred_Tair_idx] = self.state[0, 2, pred_Tair_idx]           
-            next_state[0, 1, Setpoint_Temp_idx] = setpoint_temp
-            next_state[0, 1, Air_flow_rate_idx] = air_flow_IO
+            next_state[0, 1, Setpoint_Temp_idx] = Setpoint_temp
+            next_state[0, 1, Air_flow_rate_idx] = Heating_IO
             next_state[0, 2, pred_Tair_idx] = tair_next_step
             
             self.state = next_state
 
         else:
-            self.state[0, 2, Setpoint_Temp_idx] = setpoint_temp
-            self.state[0, 2, Air_flow_rate_idx] = air_flow_IO
+            self.state[0, 2, Setpoint_Temp_idx] = Setpoint_temp
+            self.state[0, 2, Air_flow_rate_idx] = Heating_IO
  
             tair_next_step = model_Tair.predict(self.state)
             
@@ -155,8 +152,8 @@ class pred_Tair_env(gym.Env):
             next_state[0, 0, Air_flow_rate_idx] = self.state[0, 1, Air_flow_rate_idx]
             next_state[0, 0, pred_Tair_idx] = self.state[0, 1, pred_Tair_idx]           
             next_state[0, 1, pred_Tair_idx] = self.state[0, 2, pred_Tair_idx]
-            next_state[0, 1, Setpoint_Temp_idx] = setpoint_temp
-            next_state[0, 1, Air_flow_rate_idx] = air_flow_IO            
+            next_state[0, 1, Setpoint_Temp_idx] = Setpoint_temp
+            next_state[0, 1, Air_flow_rate_idx] = Heating_IO            
             next_state[0, 2, pred_Tair_idx] = tair_next_step
             
             self.state = next_state
