8493faf7a5e744cb8bf76872c324910992f4a4cd
simonme42 <57465172+simonme42@users.noreply.github.com>
2019-11-08 19:22:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='pred_tair_env',
       version='0.0.1',
-      install_requires=['gym>=0.2.3']
+      install_requires=['gym',
+                       'sklearn',
+                       'tensorflow']
 )
