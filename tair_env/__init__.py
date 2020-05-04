73631041645e52cc66b5f080001f44fc6e027898
simonme42 <57465172+simonme42@users.noreply.github.com>
2020-05-04 19:07:27 +0200

    Update __init__.py

--- tair_env/__init__.py
+++ tair_env/__init__.py
@@ -2,10 +2,5 @@ from gym.envs.registration import register
 
 register(
     id='HVACTair-v0',
-    entry_point='tair_env.envs:HVACTairEnv_1846',
-)
-
-register(
-    id='HVACTair-v1',
-    entry_point='tair_env.envs:HVACTairEnv_7390',
+    entry_point='tair_env.envs:HVACTairEnv',
 )
